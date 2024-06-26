aa7462df8959015ff4e66813390b1beb069826f1
vinkims <vincekigen@gmail.com>
2024-06-26 20:34:11 +0300

    Initial commit

---
+++ StudentPortal/StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool Subscribed { get; set; }
+    }
+}
